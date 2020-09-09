06f362ffafa41f3b38d6465cb191e4cd7851cacd
John <jjh5166@gmail.com>
2020-09-08 23:22:49 -0400

    picks controller, get mypicks

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :user, only: %i[create show update]
+      get '/mypicks' => 'picks#my_picks'
     end
   end
 end
