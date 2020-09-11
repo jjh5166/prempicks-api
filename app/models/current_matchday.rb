403db2ec8894643cc3964869c88aadf8b31cff40
John <jjh5166@gmail.com>
2020-09-10 20:53:38 -0400

    record update and destroy controls

--- app/models/current_matchday.rb
+++ app/models/current_matchday.rb
@@ -3,4 +3,6 @@
 # singleton reference to current matchday
 class CurrentMatchday < ApplicationRecord
   validates_inclusion_of :singleton_guard, in: [0]
+  attr_readonly :singleton_guard
+  before_destroy { |_record| raise ReadOnlyRecord }
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,6 @@
 # scores table updated by script
 class Score < ApplicationRecord
   belongs_to :matchday
+  attr_readonly :matchday_id, :team_id
+  before_destroy { |_record| raise ReadOnlyRecord }
 end
