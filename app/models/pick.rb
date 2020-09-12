3045b5ce957fb816e7bf4b432db874a6d6399a07
John <jjh5166@gmail.com>
2020-09-12 00:49:05 -0400

    Pick validation for team_id and lock_time

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -7,4 +7,16 @@ class Pick < ApplicationRecord
   validates :team_id, uniqueness: { scope: %i[half user_uid] }, allow_blank: true, on: :update
   default_scope { order(matchday: :asc) }
   attr_readonly :matchday, :user_uid, :half
+  validate :update_before_lock
+  validates :team_id, inclusion: { in: %w[ ARS AVL BHA BUR CHE CRY EVE FUL LEE LEI
+                                           LIV MCI MUN NEW SHU SOU TOT WBA WHU WOL ] }, allow_blank: true
+
+  private
+
+  def update_before_lock
+    lock_time = Matchday.find(matchday).lock_time
+    return if DateTime.now.utc < lock_time
+
+    errors.add 'Pick cannot be updated as matchday has started'
+  end
 end
