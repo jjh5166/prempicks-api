4ef1016e69f9b97e2f642594b01cdbd1a3b8825a
John <jjh5166@gmail.com>
2020-09-12 16:57:16 -0400

    current_matchday method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# Application Controller
 class ApplicationController < ActionController::API
+  def current_matchday
+    CurrentMatchday.find(1).matchday
+  end
 end
