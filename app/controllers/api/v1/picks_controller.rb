b25b7d22e65082d924cc77a3bfdfbb55618b571e
John <jjh5166@gmail.com>
2020-09-12 23:10:14 -0400

    standings user picks data, route

--- app/controllers/api/v1/picks_controller.rb
+++ app/controllers/api/v1/picks_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     # Picks Controller
     class PicksController < ApplicationController
-      before_action :set_picks, only: %i[my_picks update]
+      before_action :set_mypicks, only: %i[my_picks update]
       before_action :set_schedule, only: %i[my_picks]
       # GET /mypicks
       def my_picks
@@ -21,9 +21,21 @@ module Api
         end
       end
 
+      # GET /standings
+      def standings
+        return unless FirebaseIdToken::Signature.verify(params[:idToken])
+
+        currently = current_matchday
+        all_picks = standings_picks_up_to(current_matchday)
+        render json: {
+          'currentMatchday': currently,
+          'standings': all_picks
+        }
+      end
+
       private
 
-      def set_picks
+      def set_mypicks
         auth_user = FirebaseIdToken::Signature.verify(params[:idToken])
         @picks = Pick.where(user_uid: auth_user['user_id'])
       end
@@ -46,6 +58,20 @@ module Api
               ]
             )
           end
+        end
+      end
+
+      def standings_picks_up_to(matchday)
+        User.all.includes(:picks)
+            .where('picks.matchday <= ?', matchday)
+            .references(:picks)
+            .map do |user|
+          {
+            name: user.team_name,
+            picks: user.picks.map do |pick|
+              { matchday: pick.matchday, team_id: pick.team_id }
+            end
+          }
         end
       end
     end
