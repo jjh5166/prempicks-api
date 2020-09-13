b84ea2fa6c035335676232702fac2e0b0443b7b3
John <jjh5166@gmail.com>
2020-09-12 23:17:42 -0400

    refactor set_schedule, standings_picks_up_to

--- app/controllers/api/v1/picks_controller.rb
+++ app/controllers/api/v1/picks_controller.rb
@@ -41,37 +41,37 @@ module Api
       end
 
       def set_schedule
-        matches_data = FootballData::Client.matches['matches']
-        matches =
-          matches_data.sort_by { |match| [match['matchday'], match['utcDate']] }
+        matches = sorted_matches
 
         @schedule = Hash[(1..38).collect { |md| [md, []] }]
         matches.each_slice(10).with_index do |slice, i|
           slice.each do |match|
-            @schedule[i + 1].push(
-              Hash[
-                'utcDate' => match['utcDate'],
-                'status' => match['status'],
-                'away' => match['awayTeam'],
-                'home' => match['homeTeam'],
-                'score' => match['score']['fullTime']
-              ]
-            )
+            @schedule[i + 1].push(match_hash(match))
           end
         end
       end
 
+      def match_hash(match)
+        Hash[
+          'utcDate' => match['utcDate'],
+          'status' => match['status'],
+          'away' => match['awayTeam'],
+          'home' => match['homeTeam'],
+          'score' => match['score']['fullTime']
+        ]
+      end
+
+      def sorted_matches
+        matches_data = FootballData::Client.matches['matches']
+        matches_data.sort_by { |match| [match['matchday'], match['utcDate']] }
+      end
+
       def standings_picks_up_to(matchday)
-        User.all.includes(:picks)
-            .where('picks.matchday <= ?', matchday)
-            .references(:picks)
+        User.all.includes(:picks).where('picks.matchday <= ?', matchday).references(:picks)
             .map do |user|
-          {
-            name: user.team_name,
-            picks: user.picks.map do |pick|
-              { matchday: pick.matchday, team_id: pick.team_id }
-            end
-          }
+          { name: user.team_name, picks: user.picks.map do |pick|
+            { matchday: pick.matchday, team_id: pick.team_id }
+          end }
         end
       end
     end
