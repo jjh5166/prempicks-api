f7930d7a3f014e588609e35e427f78ab61c943df
John <jjh5166@gmail.com>
2020-09-14 15:19:11 -0400

    authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@
 
 # Application Controller
 class ApplicationController < ActionController::API
+  def authorize
+    auth_user = FirebaseIdToken::Signature.verify(params[:idToken])
+    @uid = auth_user ? auth_user['user_id'] : nil
+    render json: {}, status: 401 if @uid.nil?
+  end
+
   def current_matchday
     CurrentMatchday.find(1).matchday
   end
