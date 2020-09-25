e217cfc517f94779f1d55c044c09eb7dc0905042
John <jjh5166@gmail.com>
2020-09-24 21:51:52 -0400

    add reminders_queued column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_161957) do
+ActiveRecord::Schema.define(version: 2020_09_25_013425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_161957) do
     t.boolean "scored", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "reminders_queued", default: false
   end
 
   create_table "picks", force: :cascade do |t|
