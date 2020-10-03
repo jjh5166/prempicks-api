2f99aaa5c2607bf8b5e4621fd668629fd683da58
John <jjh5166@gmail.com>
2020-10-02 23:23:37 -0400

    QueueMatchdayLock job

---
+++ app/jobs/queue_matchday_lock.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# set current matchday, lock Matchday if lock_time passed
+class QueueMatchdayLock < ActiveJob::Base
+  include MatchdayHelper
+  def perform
+    mds = matchdays_to_lock
+    return unless mds.any?
+
+    mds.each do |md|
+      LockMatchday.perform_at(md.lock_time, md.id)
+    end
+  end
+end
