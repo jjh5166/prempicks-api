c0a2cf5e916c9d76921a13b0cce82535196d50f7
John <jjh5166@gmail.com>
2020-10-08 22:55:26 -0400

    factory uid alphanumeric

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
                   LIV MCI MUN NEW SHU SOU TOT WBA WHU WOL ]
 
   factory :user, class: User do
-    uid { Faker::String.random(length: 18) }
+    uid { Faker::Alphanumeric.alphanumeric(number: 18) }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     team_name { Faker::Team.name }
