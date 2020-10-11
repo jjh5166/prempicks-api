a2f5e5df2b38365f1524463d36c424b5904fba4a
John <jjh5166@gmail.com>
2020-10-10 22:23:53 -0400

    test find picks

--- spec/helpers/picks_helper_spec.rb
+++ spec/helpers/picks_helper_spec.rb
@@ -6,12 +6,20 @@ RSpec.describe PicksHelper, type: :helper do
   before do
     FactoryBot.create_list(:user, 12)
     Matchday.all.update_all(lock_time: '2030-10-31 12:30:00')
-    Pick.where(matchday: 1).limit(7).update(team_id: 'TOT')
+    Pick.where(matchday: 1).update(team_id: 'AVL')
+    Pick.where(matchday: 2).limit(7).update(team_id: 'TOT')
   end
 
   describe '#users_no_pick' do
     it 'returns picks only users without picks' do
-      expect(helper.users_no_pick(1).count).to eq(5)
+      expect(helper.users_no_pick(2).count).to eq(5)
+    end
+  end
+
+  describe '#find_pick' do
+    it 'returns next team up' do
+      test_picks = %w[CHE MUN MCI LIV ARS]
+      expect(helper.find_pick(test_picks)).to eq('LEI')
     end
   end
 end
