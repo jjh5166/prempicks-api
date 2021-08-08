8da5e1c5c2b03b7677fc7878b4946641f4966f63
John <jjh5166@gmail.com>
2021-08-09 00:25:43 +0100

    opt_in_seed_picks guard clause

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -55,10 +55,12 @@ module Api
       end
 
       def opt_in_seed_picks
-        (1..38).each do |n|
-          h = n < 20 ? 1 : 2
-          # to do: get season from cache
-          Pick.new(user_uid: @user.uid, matchday: n, half: h, season: "2021").save(validate: false)
+        if @user.picks.where(season: "2021").length === 0
+          (1..38).each do |n|
+            h = n < 20 ? 1 : 2
+            # to do: get season from cache
+            Pick.new(user_uid: @user.uid, matchday: n, half: h, season: "2021").save(validate: false)
+          end
         end
       end
     end
