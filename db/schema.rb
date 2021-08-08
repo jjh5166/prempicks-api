72d39d140acbf1a3c82c564e89323f76af80c4a1
John <jjh5166@gmail.com>
2021-08-08 21:53:26 +0100

    add live column to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_082132) do
+ActiveRecord::Schema.define(version: 2021_08_08_185556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_082132) do
     t.string "team_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "live", default: false
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
