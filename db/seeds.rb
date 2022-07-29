58bc129a1ac11e314e32e159da11e8114598f1e2
John <jjh5166@gmail.com>
2022-07-29 15:53:03 +0100

    use CURRENT_SEASON const

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@ if Matchday.none?
   # update locktimes
 end
 
-if Score.where(season: "2021").none?
+if Score.where(season: CURRENT_SEASON).none?
   teams = %w[ ARS AVL BRE BHA BUR CHE CRY EVE LEE LEI
               LIV MCI MUN NEW NOR SOU TOT WAT WHU WOL ]
   teams.each do |t|
     (1..38).each do |week|
-      Score.create(team_id: t, matchday_id: week, season: "2021")
+      Score.create(team_id: t, matchday_id: week, season: CURRENT_SEASON)
     end
   end
 end
