ab5fe8e80ab62cf02f9d7ca27b0a007a6b0dacd6
John <jjh5166@gmail.com>
2022-07-29 16:06:18 +0100

    notes in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# initial table values used for every season
+CurrentMatchday.first_or_create!(singleton_guard: 0, matchday: 1) if CurrentMatchday.none?
+
 if Matchday.none?
   (1..38).each do |week|
     Matchday.create(id: week)
@@ -7,6 +10,7 @@ if Matchday.none?
   # update locktimes
 end
 
+# pre season updates
 if Score.where(season: CURRENT_SEASON).none?
   teams = %w[ ARS AVL BRE BHA BUR CHE CRY EVE LEE LEI
               LIV MCI MUN NEW NOR SOU TOT WAT WHU WOL ]
@@ -16,5 +20,3 @@ if Score.where(season: CURRENT_SEASON).none?
     end
   end
 end
-
-CurrentMatchday.first_or_create!(singleton_guard: 0, matchday: 1) if CurrentMatchday.none?
