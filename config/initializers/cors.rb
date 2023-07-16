b05cb0642904af4f00cd4031caeded1d7654f03f
John <jjh5166@gmail.com>
2023-07-16 18:10:48 +0100

    update CORS config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,10 +2,19 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('ALLOWED_ORIGINS')
+    origins ENV.fetch('ALLOWED_ORIGINS') || raise("Missing env variable ALLOWED_ORIGINS")
 
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+    resource '/api/*',
+            headers: :any,
+            methods: %i[get post put patch delete options head]
+    resource '/rails/*',
+            headers: :any,
+            methods: %i[get post put patch delete options head]
+  end
+
+  allow do
+    origins '*'
+
+    resource '/sidekiq/*', headers: :any, methods: :get
   end
 end
