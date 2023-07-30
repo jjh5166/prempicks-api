d2adbbad2b0db7cdd4e6c441b1f771d9886caf93
John <jjh5166@gmail.com>
2023-07-30 16:47:09 +0100

    epl schedule api route

--- config/routes.rb
+++ config/routes.rb
@@ -8,21 +8,24 @@ Rails.application.routes.draw do
       patch '/mypicks' => 'picks#update'
       get '/standings' => 'picks#standings'
       get '/score-matchday' => 'scores#trigger_score_matchday'
-      post 'user/opt-in'=> 'users#opt_in'
+      post 'user/opt-in' => 'users#opt_in'
+      get 'epl/schedule' => 'football_api#schedule'
     end
   end
 
   require 'sidekiq/web'
   require 'sidekiq-scheduler/web'
-  
+
   # Configure Sidekiq-specific session middleware
   Sidekiq::Web.use ActionDispatch::Cookies
-  Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+  Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
 
   if Rails.env.production?
     Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
-        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
+                                                  ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
+                                                    ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
     end
   end
   mount Sidekiq::Web, at: '/sidekiq'
