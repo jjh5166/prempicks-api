e0b98a6b4c1e4208f9a91db4e3e30b4fab3a6b65
John <jjh5166@gmail.com>
2024-08-19 14:43:44 +0100

    Revert "use empthy syntax"
    This reverts commit 62e056c0b3b45b6fe1b37039605c14f3b41e3579.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -64,7 +64,7 @@ module Api
       end
 
       def opt_in_seed_picks
-        return unless @user.picks.where(season: CURRENT_SEASON).empty?
+        return unless @user.picks.where(season: CURRENT_SEASON).length === 0
 
         (1..38).each do |n|
           h = n < 20 ? 1 : 2
