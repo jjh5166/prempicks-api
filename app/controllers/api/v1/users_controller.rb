76f6bdcf154d5afe092aa5034063cb28204206bb
John <jjh5166@gmail.com>
2024-08-23 11:42:40 +0100

    autopick on late signup, add empty condition

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,6 +19,7 @@ module Api
         # to do: use static value in new method
         @user.update(live: true)
         if @user.save
+          auto_pick_locked_mds
           # TODO: send welcome email
           render json: @user, status: :created
         else
@@ -76,6 +77,8 @@ module Api
       # auto pick up to catch users picks up to time of opt-in / sign-up
       def auto_pick_locked_mds
         locked_mds = Matchday.where(locked: true).pluck(:id)
+
+        return if locked_mds.empty?
 
         locked_mds.each do |matchday|
           auto_pick(matchday, [@user])
